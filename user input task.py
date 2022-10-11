36aa2075caedd9b8c72d999ad86cc909f74ea3a1
Ayoub-Igo10 <ayoub.igo10@outlook.com>
2022-10-11 14:32:40 +0100

    task

---
+++ user input task.py
@@ -0,0 +1,19 @@
+print("Good Morning, Please enter your First Name")
+first_name = input()
+#
+print ("Enter Last Name")
+last_name = input()
+#
+print("Enter DOB")
+DOB = input()
+
+print("Are you a Uk resident? Answer Yes or No")
+uk_resident = input()
+
+print("Do you enjoy football Answer Yes or No")
+Hobbies = input()
+
+Hobbies=input("Can you please enter your Hobbies")
+
+
+print(f'Hello   {first_name}  {last_name} {DOB} {uk_resident} {Hobbies}')
