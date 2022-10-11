aaf863955e111f835a38e839bbc5a3388cd2e023
Ayoub-Igo10 <ayoub.igo10@outlook.com>
2022-10-11 15:05:12 +0100

    Tuples

---
+++ Data collections.py
@@ -0,0 +1,31 @@
+# # Data Collections
+# # Lists
+# # Tuples
+# # Dict
+#
+# # lists
+# # syntax list_name = ["sfksdf", "dkfsdf"]
+#
+# # apply DRY
+# shopping_list = ["ketchup", "fanta", "eggs", "bread"]
+#     # indexing      0          1        2        3
+# # print(shopping_list)
+# # print(type(shopping_list))
+# shopping_list.append("chicken")
+# print(shopping_list)_# add an item to the list
+# print(shopping_list[2])
+# shopping_list[2] = "icecream"
+# print(shopping_list)
+# # find out how to remove an item from the list
+# # find out how to remove fanta from list
+# shopping_list[0] = "fanta"
+# shopping_list.remove("fanta")
+# print(shopping_list)
+
+
+
+
+# Can list have multiple data types
+multiple_type = [1, 2, 3, "one", "five", "ten"]
+print(multiple_type)
+print(multiple_type[2])
--- user input task.py
+++ user input task.py
@@ -1,19 +1,25 @@
-print("Good Morning, Please enter your First Name")
-first_name = input()
+# print("Hello, What is your name")
+# first_name = input() # Takes the input from the user and stores in the variable
+# print("Please enter your last name")
+# last_name = input() # Takes the input from the user and stores in the variable
+# print("Thank you and nice to meet you", first_name, last_name)
 #
-print ("Enter Last Name")
-last_name = input()
+# print("What is your date of birth")
+# DOB = input()
 #
-print("Enter DOB")
-DOB = input()
-
-print("Are you a Uk resident? Answer Yes or No")
-uk_resident = input()
-
-print("Do you enjoy football Answer Yes or No")
-Hobbies = input()
-
-Hobbies=input("Can you please enter your Hobbies")
-
-
-print(f'Hello   {first_name}  {last_name} {DOB} {uk_resident} {Hobbies}')
+# print("What is your door number")
+# door_number = input()
+#
+# print("What is the first line of your address")
+# address = input()
+#
+# print("What are your hobbies?")
+# hobbies = input()
+#
+#
+# print("Nice to meet you, below are your details")
+# print("First name:" , first_name)
+# print("Last name", last_name)
+# print("D.O.B;", DOB)
+# print(f"Address: {door_number} {address}")
+# print("Hobbies", hobbies)
